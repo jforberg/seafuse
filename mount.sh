a732b9461b769eeaa0c83020a126f827bf0d9b40
Johan Förberg <johan@forberg.se>
2025-08-12 15:02:21 +0200

    Fix mount script

--- mount.sh
+++ mount.sh
@@ -2,8 +2,7 @@
 
 set -eux
 
-cargo build
-
 trap 'fusermount -u mnt' exit
 
-target/debug/seafrepo --verbose mount tests/data/testrepo 868be3a7-b357-4189-af52-304b402d9904 mnt
+cargo run mount -v tests/testrepos/multiblock 868be3a7-b357-4189-af52-304b402d9904 mnt
+
